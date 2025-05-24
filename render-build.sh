2af5a2a4395aa9b7d7606fcbf28df576d27b7ad7
Arpan Patra <arpanpatra800188500@gmail.com>
2025-05-24 20:47:29 +0530

    Update render-build.sh5

--- render-build.sh
+++ render-build.sh
@@ -4,11 +4,10 @@ echo "=== Installing Node.js dependencies ==="
 npm install
 
 echo "=== Installing Python dependencies ==="
+which python3
+python3 --version
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 echo "=== Installed Python packages ==="
 pip3 list
-
-echo "=== Python path ==="
-which python3
